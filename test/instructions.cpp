e9e968feca0c6741e2bbfb13742cd9c4370feb0f
Zofyan <sofyan_aarrass@icloud.com>
2022-08-28 22:57:15 +0200

    tests added

--- test/instructions.cpp
+++ test/instructions.cpp
@@ -7,6 +7,8 @@
 #include "../include/instructions/arm_instruction.h"
 #include "test.h"
 
+#define BEFORE_TEST(inst, a) (*(inst) = mul_instruction_int{2, 0b1001, 1, 0, 0, 0, 0, 0, a}.mul_instruction_int_t)
+
 TEST_CASE("testing the condition flags") {
     Cpu cpu = Cpu(nullptr);
     *cpu.registers.r00 = 0;
@@ -16,54 +18,217 @@ TEST_CASE("testing the condition flags") {
     uint32_t inst;
     ArmInstruction *instruction;
 
-    #define BEFORE_TEST(inst, a) (*(inst) = mul_instruction_int{2, 0b1001, 1, 0, 0, 0, 0, 0, a}.mul_instruction_int_t)
-
-    SUBCASE("instruction does not run"){
+    SUBCASE("instruction does not run") {
         BEFORE_TEST(&inst, 0b0000);
         cpu.flags->z = 0;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 0);
+        SUBCASE("condition EQ") { CHECK(*cpu.registers.r00 == 0); }
         BEFORE_TEST(&inst, 0b0001);
         cpu.flags->z = 1;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 0);
+        SUBCASE("condition NE") { CHECK(*cpu.registers.r00 == 0); }
         BEFORE_TEST(&inst, 0b0010);
         cpu.flags->c = 0;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 0);
+        SUBCASE("condition CS/HS") { CHECK(*cpu.registers.r00 == 0); }
         BEFORE_TEST(&inst, 0b0011);
         cpu.flags->c = 1;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 0);
+        SUBCASE("condition CC/LO") { CHECK(*cpu.registers.r00 == 0); }
+        BEFORE_TEST(&inst, 0b0100);
+        cpu.flags->n = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition MI") { CHECK(*cpu.registers.r00 == 0); }
+        BEFORE_TEST(&inst, 0b0101);
+        cpu.flags->n = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition PL") { CHECK(*cpu.registers.r00 == 0); }
+        BEFORE_TEST(&inst, 0b0110);
+        cpu.flags->v = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition VS") { CHECK(*cpu.registers.r00 == 0); }
+        BEFORE_TEST(&inst, 0b0111);
+        cpu.flags->v = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition VC") { CHECK(*cpu.registers.r00 == 0); }
 
+        BEFORE_TEST(&inst, 0b1000);
+        cpu.flags->c = 1;
+        cpu.flags->z = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition HI") { CHECK(*cpu.registers.r00 == 0); };
+
+        BEFORE_TEST(&inst, 0b1001);
+        cpu.flags->c = 1;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LS") { CHECK(*cpu.registers.r00 == 0); }
+
+        BEFORE_TEST(&inst, 0b1010);
+        cpu.flags->n = 0;
+        cpu.flags->v = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GE") { CHECK(*cpu.registers.r00 == 0); }
+
+        BEFORE_TEST(&inst, 0b1011);
+        cpu.flags->n = 1;
+        cpu.flags->v = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LT") { CHECK(*cpu.registers.r00 == 0); }
+
+        BEFORE_TEST(&inst, 0b1100);
+        cpu.flags->n = 1;
+        cpu.flags->v = 1;
+        cpu.flags->z = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GT") { CHECK(*cpu.registers.r00 == 0); }
+        BEFORE_TEST(&inst, 0b1100);
+        cpu.flags->n = 0;
+        cpu.flags->v = 1;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GT") { CHECK(*cpu.registers.r00 == 0); }
+
+        BEFORE_TEST(&inst, 0b1101);
+        cpu.flags->n = 0;
+        cpu.flags->v = 0;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LE") { CHECK(*cpu.registers.r00 == 0); }
     }
 
-    SUBCASE("instruction does run"){
+    SUBCASE("instruction does run") {
         BEFORE_TEST(&inst, 0b0000);
         cpu.flags->z = 1;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 30);
+        SUBCASE("condition EQ") { CHECK(*cpu.registers.r00 == 30); }
         BEFORE_TEST(&inst, 0b0001);
         cpu.flags->z = 0;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 30);
+        SUBCASE("condition NE") { CHECK(*cpu.registers.r00 == 30); }
         BEFORE_TEST(&inst, 0b0010);
         cpu.flags->c = 1;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 30);
+        SUBCASE("condition CS/HS") { CHECK(*cpu.registers.r00 == 30); }
         BEFORE_TEST(&inst, 0b0011);
         cpu.flags->c = 0;
         instruction = ArmInstruction::GetInstruction(inst, &cpu);
         instruction->run();
-        CHECK(*cpu.registers.r00 == 30);
+        SUBCASE("condition CC/LO") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b0100);
+        cpu.flags->n = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition MI") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b0101);
+        cpu.flags->n = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition PL") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b0110);
+        cpu.flags->v = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition VS") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b0111);
+        cpu.flags->v = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition VC") { CHECK(*cpu.registers.r00 == 30); }
 
+        BEFORE_TEST(&inst, 0b1000);
+        cpu.flags->c = 1;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition HI") { CHECK(*cpu.registers.r00 == 30); };
+
+        BEFORE_TEST(&inst, 0b1001);
+        cpu.flags->c = 0;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LS") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b1001);
+        cpu.flags->c = 1;
+        cpu.flags->z = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LS") { CHECK(*cpu.registers.r00 == 30); }
+
+        BEFORE_TEST(&inst, 0b1010);
+        cpu.flags->n = 1;
+        cpu.flags->v = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GE") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b1010);
+        cpu.flags->n = 0;
+        cpu.flags->v = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GE") { CHECK(*cpu.registers.r00 == 30); }
+
+        BEFORE_TEST(&inst, 0b1011);
+        cpu.flags->n = 1;
+        cpu.flags->v = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LT") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b1011);
+        cpu.flags->n = 0;
+        cpu.flags->v = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LT") { CHECK(*cpu.registers.r00 == 30); }
+
+        BEFORE_TEST(&inst, 0b1100);
+        cpu.flags->n = 1;
+        cpu.flags->v = 1;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GT") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b1100);
+        cpu.flags->n = 0;
+        cpu.flags->v = 0;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition GT") { CHECK(*cpu.registers.r00 == 30); }
+
+        BEFORE_TEST(&inst, 0b1101);
+        cpu.flags->n = 1;
+        cpu.flags->v = 0;
+        cpu.flags->z = 0;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LE") { CHECK(*cpu.registers.r00 == 30); }
+        BEFORE_TEST(&inst, 0b1101);
+        cpu.flags->n = 0;
+        cpu.flags->v = 0;
+        cpu.flags->z = 1;
+        instruction = ArmInstruction::GetInstruction(inst, &cpu);
+        instruction->run();
+        SUBCASE("condition LE") { CHECK(*cpu.registers.r00 == 30); }
     }
 }
 
@@ -76,12 +241,12 @@ TEST_CASE("testing the BX instruction") {
     inst = 0b00000001001011111111111100010001;
     instruction = ArmInstruction::GetInstruction(inst, &cpu);
     instruction->run();
-    CHECK(cpu.cpu_mode == THUMB);
+    SUBCASE("condition ") { CHECK(cpu.cpu_mode == THUMB); }
 
     inst = 0b00000001001011111111111100010000;
     instruction = ArmInstruction::GetInstruction(inst, &cpu);
     instruction->run();
-    CHECK(cpu.cpu_mode == ARM);
+    SUBCASE("condition ") { CHECK(cpu.cpu_mode == ARM); }
 
 
 }
@@ -108,7 +273,7 @@ TEST_CASE("testing the MUL instruction") {
 
     instruction = ArmInstruction::GetInstruction(inst, &cpu);
     instruction->run();
-    CHECK(*cpu.registers.r00 == 30);
+    SUBCASE("condition ") { CHECK(*cpu.registers.r00 == 30); }
 
     inst = mul_instruction_int{2,
                                0b1001,
@@ -121,7 +286,7 @@ TEST_CASE("testing the MUL instruction") {
                                0}.mul_instruction_int_t;
     instruction = ArmInstruction::GetInstruction(inst, &cpu);
     instruction->run();
-    CHECK(*cpu.registers.r00 == 70);
+    SUBCASE("condition ") { CHECK(*cpu.registers.r00 == 70); }
     CHECK((cpu.flags->n == 0));
     CHECK((cpu.flags->z == 0));
 
@@ -153,7 +318,7 @@ TEST_CASE("testing the MUL instruction") {
                                0}.mul_instruction_int_t;
     instruction = ArmInstruction::GetInstruction(inst, &cpu);
     instruction->run();
-    CHECK(*cpu.registers.r00 == 0);
+    SUBCASE("condition ") { CHECK(*cpu.registers.r00 == 0); }
     CHECK((cpu.flags->n == 0));
     CHECK((cpu.flags->z == 1));
 }
